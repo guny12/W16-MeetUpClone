3d523cff3848bf3951dcaf8fa5762841fcd36a84
Jim <Jimjnguy@gmail.com>
2021-03-25 17:26:26 -0400

    added login validators to rooutes/api/session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,11 +2,20 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
+
+const validateLogin = [
+	check("credential").exists({ checkFalsy: true }).notEmpty().withMessage("Please provide a valid email or username."),
+	check("password").exists({ checkFalsy: true }).withMessage("Please provide a password."),
+	handleValidationErrors,
+];
 
 // Log in
 router.post(
 	"/",
+	validateLogin,
 	asyncHandler(async (req, res, next) => {
 		const { credential, password } = req.body;
 		const user = await User.login({ credential, password });
@@ -20,9 +29,7 @@ router.post(
 		}
 
 		await setTokenCookie(res, user);
-		return res.json({
-			user,
-		});
+		return res.json({ user });
 	})
 );
 
