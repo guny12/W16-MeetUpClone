be91d686fa9842f6c7e1dc3ba6e8c87abf1672c2
Jim <Jimjnguy@gmail.com>
2021-03-29 23:04:47 -0400

    added seed data for events

---
+++ backend/db/seeders/20210330025825-events.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert(
+			"Events",
+			[
+				{
+					name: "Sushi cook off",
+					hostId: 4,
+					groupId: 1,
+					location: faker.address.streetAddress(),
+					description: faker.lorem.paragraph(),
+					eventDate: faker.date.soon(),
+					eventType: "cook off",
+					availableSpots: faker.random.number(),
+					imgURL: faker.image.imageUrl(),
+				},
+				{
+					name: "Robatayaki cook off",
+					hostId: 4,
+					groupId: 1,
+					location: faker.address.streetAddress(),
+					description: faker.lorem.paragraph(),
+					eventDate: faker.date.soon(),
+					eventType: "cook off",
+					availableSpots: faker.random.number(),
+					imgURL: faker.image.imageUrl(),
+				},
+				{
+					name: "Casual experimentation",
+					hostId: 4,
+					groupId: 3,
+					location: faker.address.streetAddress(),
+					description: faker.lorem.paragraph(),
+					eventDate: faker.date.soon(),
+					eventType: "experimentation",
+					availableSpots: faker.random.number(),
+					imgURL: faker.image.imageUrl(),
+				},
+			],
+			{}
+		);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete("Events", null, {});
+	},
+};
